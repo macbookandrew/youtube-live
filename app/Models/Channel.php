4b9a9754ba292b9881d677242fdd9db1868d8114
Andrew Minion <andrew@andrewrminion.com>
2024-09-02 19:34:03 -0500

    add channel model

---
+++ app/Models/Channel.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Mtvs\EloquentHashids\HasHashid;
+use Mtvs\EloquentHashids\HashidRouting;
+
+/**
+ * @property int $id
+ * @property string $google_channel_id
+ * @property string $name
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ *
+ * @method static \Database\Factories\ChannelFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Channel newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Channel newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Channel query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Channel whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Channel whereGoogleChannelId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Channel whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Channel whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Channel whereUpdatedAt($value)
+ *
+ * @mixin \Eloquent
+ */
+class Channel extends Model
+{
+    use HasFactory;
+    use HasHashid;
+    use HashidRouting;
+}
---
+++ database/factories/ChannelFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Channel>
+ */
+class ChannelFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'google_channel_id' => fake()->word(),
+            'name' => fake()->company(),
+        ];
+    }
+}
