3aed5a49acfcbe2438a488f64a2a01d664cb0d64
Andrew Minion <andrew@andrewrminion.com>
2024-09-03 21:33:38 -0500

    remove unnecessary Google SDK

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,5 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
-    App\Providers\GoogleServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,
 ];
