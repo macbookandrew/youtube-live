cf9d747d6619390f938c0e449036db32c1cc10d3
Andrew Minion <andrew@andrewrminion.com>
2024-09-03 20:50:42 -0500

    fake HTTP request instead of the cache

--- tests/Feature/Models/ChannelTest.php
+++ tests/Feature/Models/ChannelTest.php
@@ -3,8 +3,10 @@
 namespace Tests\Feature\Models;
 
 use App\Models\Channel;
+use Illuminate\Http\Client\Request;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
 class ChannelTest extends TestCase
@@ -13,40 +15,35 @@ class ChannelTest extends TestCase
     {
         $channel = Channel::factory()->create();
 
-        Cache::shouldReceive('remember')
-            ->once()
-            ->withSomeOfArgs($channel->cache_key)
-            ->andReturn(collect());
+        Http::fake(['https://www.googleapis.com/youtube/v3/search*' => []]);
 
         $this->assertFalse($channel->is_live);
+
+        Http::assertSent(fn (Request $request) => str($request->url())->contains($channel->google_channel_id));
     }
 
     public function test_is_live_attribute_for_some_items(): void
     {
         $channel = Channel::factory()->create();
 
-        Cache::shouldReceive('remember')
-            ->once()
-            ->withSomeOfArgs($channel->cache_key)
-            ->andReturn(collect(
-                File::json(base_path('/tests/data/youtube-data-api/search-results.json'))['items']
-            ));
+        Http::fake(['https://www.googleapis.com/youtube/v3/search*' => File::json(base_path('/tests/data/youtube-data-api/search-results.json'))]);
 
         $this->assertTrue($channel->is_live);
+
+        Http::assertSent(fn (Request $request) => str($request->url())->contains($channel->google_channel_id));
     }
 
     public function test_current_video_id_attribute(): void
     {
         $channel = Channel::factory()->create();
+        $response = File::json(base_path('/tests/data/youtube-data-api/search-results.json'));
+        $items = $response['items'];
 
-        Cache::shouldReceive('remember')
-            ->once()
-            ->withSomeOfArgs($channel->cache_key)
-            ->andReturn(collect(
-                $items = File::json(base_path('/tests/data/youtube-data-api/search-results.json'))['items']
-            ));
+        Http::fake(['https://www.googleapis.com/youtube/v3/search*' => $response]);
 
         $this->assertEquals($items[0]['id']['videoId'], $channel->current_video_id);
+
+        Http::assertSent(fn (Request $request) => str($request->url())->contains($channel->google_channel_id));
     }
 
     public function test_flush_cache(): void
