916da65e0bab5bacfdf3a6276d58ccd1bf55998a
Andrew Minion <andrew@andrewrminion.com>
2024-09-04 08:35:51 -0500

    fix dimensions

--- resources/views/live-controller.blade.php
+++ resources/views/live-controller.blade.php
@@ -12,10 +12,19 @@
     @elseif ($fallbackVideo)
         {!! $fallbackVideo !!}
     @elseif ($fallbackImage)
-        <img src="{{ $fallbackImage }}" />
+        <img src="{{ $fallbackImage }}" style="width:100%; height:auto;" />
     @else
         No live content; please check back later.
     @endif
+
+    <script>
+        window.addEventListener('load', function() {
+            window.top.postMessage({
+                height: document.body.scrollHeight,
+                width: document.body.scrollWidth
+            }, '*');
+        });
+    </script>
 </body>
 
 </html>
